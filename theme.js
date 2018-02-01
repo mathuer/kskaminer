5effc3a8332e14dbd1e32939f0a32373f96b6417
Malin <mal93in@gmail.com>
2018-02-01 15:58:43 +0100

    update theme

--- theme.js
+++ theme.js
@@ -23,9 +23,59 @@ Bliss.Theme.HeaderScroll = function() {
 	});
 };
 
+Bliss.Theme.HandleContactForm = function() {
+	$(function() {                  
+	  $(".contact-button").click(function() {  
+	    $(".contact-form").addClass("show-contact-form"); 
+	    $(".contact-button").addClass("contact-button-hide");     
+	  });
+	  $(".close-form").click(function() {  
+	    $(".contact-form").removeClass("show-contact-form");
+	    $(".contact-button").removeClass("contact-button-hide");   
+	  });
+	});
+	
+	$(document).on("submit", ".contact-form form", function(e){
+		e.preventDefault();
+		$.fancybox.showLoading();
+		var form_wrapper = $(this).parent();
+		var form = $(this);
+		var data = form.serialize();
+
+		$.ajax( {
+			type:"POST",
+			data: data,
+		} )
+		.done(function(data){
+			try {
+			    result = JSON.parse(data);
+			} catch(e) {
+				console.log(data);
+				return;
+			}
+
+			if( result["status"] == 0 ){
+				$(".errors", form_wrapper).empty();
+				$(".errors", form_wrapper).append( result["error_markup"] );
+			}else if( result["status"] == 1 ){
+				$(".errors", form_wrapper).empty();
+				$(".errors", form_wrapper).append( result["error_markup"] );
+				form.slideUp();
+				setTimeout(function(){
+					$(".contact-form").removeClass("show-contact-form");    
+				}, 1500)
+			}
+			$.fancybox.hideLoading();
+		});	
+
+	});
+};
+
+
 
 // Create an instace of your function.
 $(function($) {
 	Bliss.Theme.HandleScroll();
 	Bliss.Theme.HeaderScroll();
+	Bliss.Theme.HandleContactForm();
 });
