3ca0583d230333737c999e04cca8fadaafdcd293
Malin <mal93in@gmail.com>
2018-02-06 14:19:53 +0100

    update theme, send to customer

--- theme.js
+++ theme.js
@@ -12,13 +12,15 @@ Bliss.Theme.HandleScroll = function() {
 };
 
 
-Bliss.Theme.HeaderScroll = function() {
+Bliss.Theme.HandleHeaderScroll = function() {
 	$(window).scroll(function(){
 		var scroll = $(window).scrollTop();
-		if(scroll >= 50){
+		if(scroll >= 1){
 			$("#header").addClass("header-scroll");
+			 $("#slideshow-wrapper").addClass("scroll");
 		}else{
 			$("#header").removeClass("header-scroll");
+			$("#slideshow-wrapper").removeClass("scroll");
 		}
 	});
 };
@@ -71,11 +73,58 @@ Bliss.Theme.HandleContactForm = function() {
 	});
 };
 
+Bliss.Theme.HandleColumnTransform = function(){
+	if( !$("#content-wrapper .page").length ){
+		return;
+	}
+	
+	$(".column-transform").each(function(){
+		var column = $(this);
+		var class_list = $(this).attr("class").split(" ");
+
+		column_class = class_list[class_list.length-1];
+		
+		var split = column_class.split("-");
+		var col_size = split[split.length-1];
+		
+					
+		column.removeClass( column_class );
+		column.addClass( "col-md-" + col_size);
+
+
+	});
+}
+
+Bliss.Theme.HandleLinkBox = function(){
+	if( $("#content-wrapper .page-editor").length > 0 ){
+		return;
+	}
+	
+	var link_boxes = $(".link-box");
+	
+	if( !link_boxes.length ){
+		return;
+	}
+	
+	link_boxes.each(function(){
+		var item = $(this);
+		
+		var link =item.find("a");
+		link.contents().unwrap();
+		if( item.hasClass("blank") ){
+			item.wrap("<a href='" + link.attr("href") + "' target='_blank'>");			
+		}else{
+			item.wrap("<a href='" + link.attr("href") + "'>");						
+		}
+	})
+}
 
 
 // Create an instace of your function.
 $(function($) {
 	Bliss.Theme.HandleScroll();
-	Bliss.Theme.HeaderScroll();
+	Bliss.Theme.HandleHeaderScroll();
 	Bliss.Theme.HandleContactForm();
+	Bliss.Theme.HandleColumnTransform();
+	Bliss.Theme.HandleLinkBox();
 });
